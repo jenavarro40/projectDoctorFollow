3058b154e0e19602aa3a5fadb527a2c93c954eb5
jenavarro40 <jenavarro40@hotmail.com>
2025-06-19 21:32:18 -0700

    Begin doctor Page

--- app/src/main/java/com/example/sportdoctorfollow/FoodActivity.kt
+++ app/src/main/java/com/example/sportdoctorfollow/FoodActivity.kt
@@ -1,17 +1,20 @@
 package com.example.sportdoctorfollow
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ListofActivities : AppCompatActivity() {
+class FoodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_listof_activities)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        this.enableEdgeToEdge()
+        setContentView(R.layout.activity_food)
+        ViewCompat.setOnApplyWindowInsetsListener(
+            findViewById(R.id.main)
+        ) { v: View, insets: WindowInsetsCompat ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
